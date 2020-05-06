614302d2154ad547ffc01b65fcec7d48df5ff2a3
Magnus <minemail@gmail.com>
2020-05-06 14:13:02 +0200

    OverviewViewModel

---
+++ Garage2_0/Models/ViewModels/OwerviewViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Garage2_0.Models.ViewModels
+{
+    public class OwerviewViewModel
+    {
+        public VehicleType VType { get; set; }
+        public string RegistrationNumber { get; set; }
+        public DateTime Arrival { get; set; }
+
+    }
+}
