ffff2825c5f752021900e3bf62e079a7d57a3dc7
Mahe <mahe22mca@gmail.com>
2020-05-06 16:22:23 +0200

    Receipt

---
+++ Garage2_0/Models/ViewModels/ReceiptViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Garage2_0.Models.ViewModels
+{
+    public class ReceiptViewModel
+    {
+        public string RegistrationNumber { get; set; }
+
+        public int ID { get; set; }
+        public DateTime Arrival { get; set; }
+
+        public DateTime CheckOut { get; set; }
+        
+        public int Price { get; set; }
+        
+
+
+    }
+}
