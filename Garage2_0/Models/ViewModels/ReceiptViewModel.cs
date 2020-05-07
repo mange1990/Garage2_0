c2a16b27de6cc81ed5265a24a996ba399830b4ab
Mahe <mahe22mca@gmail.com>
2020-05-07 11:58:21 +0200

    added cost&datetime

--- Garage2_0/Models/ViewModels/ReceiptViewModel.cs
+++ Garage2_0/Models/ViewModels/ReceiptViewModel.cs
@@ -8,17 +8,21 @@ namespace Garage2_0.Models.ViewModels
 {
     public class ReceiptViewModel
     {
-        [Display(Name = "Registration number")]
+        public int ID { get; set; }
 
+        [Display(Name = "Registration number")]
         public string RegistrationNumber { get; set; }
 
-        public int ID { get; set; }
+        [DisplayFormat(DataFormatString = "{0:yyyy:MM:dd HH:mm:ss}")]
         [Display(Name = "Vehicle parked time")]
-
         public DateTime Arrival { get; set; }
 
+
+        [DisplayFormat(DataFormatString = "{0:yyyy:MM:dd HH:mm:ss}")]
+        [Display(Name = "Vehicle checked out time")]
         public DateTime CheckOut { get; set; }
-        
+
+        [Display(Name = "The Cost")]
         public int Price { get; set; }
         
 
