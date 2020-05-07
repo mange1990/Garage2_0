dbac8fb33e8e633d9d65e4eda3c26d8e25a0eb97
Mahe <mahe22mca@gmail.com>
2020-05-07 16:49:30 +0200

    receiptoverall

--- Garage2_0/Models/ViewModels/ReceiptViewModel.cs
+++ Garage2_0/Models/ViewModels/ReceiptViewModel.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
+
 namespace Garage2_0.Models.ViewModels
 {
     public class ReceiptViewModel
@@ -21,11 +22,14 @@ namespace Garage2_0.Models.ViewModels
         [DisplayFormat(DataFormatString = "{0:yyyy:MM:dd HH:mm:ss}")]
         [Display(Name = "Vehicle checked out time")]
         public DateTime CheckOut { get; set; }
-
-        [Display(Name = "The Cost")]
-        public int Price { get; set; }
         
+        [DisplayFormat(DataFormatString = "{0} kr")]
+        [Display(Name = "Total Price")]
+        public int Price { get; set; }
 
+        [DisplayFormat(DataFormatString = "{0} hours")]
+        [Display(Name = "Total Time")]
+        public int ParkingTime { get; set; }
 
     }
 }
