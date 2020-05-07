f36e824fb16348b0a8390f05d07e40e63cd0fb7a
Mahe <mahe22mca@gmail.com>
2020-05-07 11:05:09 +0200

    displayname

--- Garage2_0/Models/ViewModels/ReceiptViewModel.cs
+++ Garage2_0/Models/ViewModels/ReceiptViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +8,13 @@ namespace Garage2_0.Models.ViewModels
 {
     public class ReceiptViewModel
     {
+        [Display(Name = "Registration number")]
+
         public string RegistrationNumber { get; set; }
 
         public int ID { get; set; }
+        [Display(Name = "Vehicle parked time")]
+
         public DateTime Arrival { get; set; }
 
         public DateTime CheckOut { get; set; }
