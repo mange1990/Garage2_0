1bee768aff3709e7179309f6d32859897b0db922
Magnus <minemail@gmail.com>
2020-05-08 11:00:26 +0200

    Registration number

--- Garage2_0/Models/ViewModels/ReceiptViewModel.cs
+++ Garage2_0/Models/ViewModels/ReceiptViewModel.cs
@@ -1,22 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+
 
 namespace Garage2_0.Models.ViewModels
 {
     public class ReceiptViewModel
     {
+        public int ID { get; set; }
+
+        [Display(Name = "Registration number")]
         public string RegistrationNumber { get; set; }
 
-        public int ID { get; set; }
+        [DisplayFormat(DataFormatString = "{0:yyyy:MM:dd HH:mm:ss}")]
+        [Display(Name = "Vehicle parked time")]
         public DateTime Arrival { get; set; }
 
+
+        [DisplayFormat(DataFormatString = "{0:yyyy:MM:dd HH:mm:ss}")]
+        [Display(Name = "Vehicle checked out time")]
         public DateTime CheckOut { get; set; }
         
+        [DisplayFormat(DataFormatString = "{0} kr")]
+        [Display(Name = "Total Price")]
         public int Price { get; set; }
-        
 
+        [DisplayFormat(DataFormatString = "{0} hours")]
+        [Display(Name = "Total Time")]
+        public int ParkingTime { get; set; }
 
     }
 }
